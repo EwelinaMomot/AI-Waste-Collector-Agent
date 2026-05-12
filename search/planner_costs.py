6871e3be37a53364d460fd66242e0c3c70ec2b06
Martyna Grochocińska <martynagrochocinska@192.168.1.14>
2026-05-12 23:09:13 +0200

    przenioslam te stale do osobnego pliku aby nie powielac importow z main

---
+++ search/planner_costs.py
@@ -0,0 +1,12 @@
+ACTION_COSTS = {
+    "przód": 1,
+    "obrót w lewo": 1,
+    "obrót w prawo": 1,
+}
+
+CELL_ENTRY_COSTS = {
+    "grass": 1,
+    "house": 10,
+    "dumpster": 5,
+    "station": 5,
+}
